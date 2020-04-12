08504140a76881c5ac1e98058e3af100a7ba23fb
Aidar Shagiyev <ipodaidar@gmail.com>
2020-04-13 01:23:22 +0300

    Tweaked board.cells

--- game/game.py
+++ game/game.py
@@ -1,3 +1,4 @@
+import itertools
 import kivy
 
 kivy.require("1.11.1")
@@ -40,15 +41,15 @@ class CellDigit(TextInput):
 
         for i in range(9):
             if value:
-                board.cells[i][cell.col].bg_color[-1] *= alpha_change
-                board.cells[i][cell.col].bg_color[1] *= green_change
-                board.cells[cell.row][i].bg_color[-1] *= alpha_change
-                board.cells[cell.row][i].bg_color[1] *= green_change
+                board.cells[i, cell.col].bg_color[-1] *= alpha_change
+                board.cells[i, cell.col].bg_color[1] *= green_change
+                board.cells[cell.row, i].bg_color[-1] *= alpha_change
+                board.cells[cell.row, i].bg_color[1] *= green_change
             else:
-                board.cells[i][cell.col].bg_color[-1] /= alpha_change
-                board.cells[i][cell.col].bg_color[1] /= green_change
-                board.cells[cell.row][i].bg_color[-1] /= alpha_change
-                board.cells[cell.row][i].bg_color[1] /= green_change
+                board.cells[i, cell.col].bg_color[-1] /= alpha_change
+                board.cells[i, cell.col].bg_color[1] /= green_change
+                board.cells[cell.row, i].bg_color[-1] /= alpha_change
+                board.cells[cell.row, i].bg_color[1] /= green_change
 
         if value and self.text:
             board.highlight_digit = self.text
@@ -105,8 +106,8 @@ class Cell(RelativeLayout):
             board = box.parent
             for i in range(9):
                 for other in [
-                    board.cells[self.row][i],
-                    board.cells[i][self.col],
+                    board.cells[self.row, i],
+                    board.cells[i, self.col],
                     box.children[i],
                 ]:
                     if other is not self and other.digit.text == self.digit.text:
@@ -125,6 +126,7 @@ class Cell(RelativeLayout):
     def clean(self):
         self.digit.text = ""
         self.guesses = self.__class__.guesses.defaultvalue
+        self.collisions = []
 
 
 class Board(GridLayout):
@@ -133,27 +135,22 @@ class Board(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cells = {}
-        for row in range(9):
-            self.cells[row] = {}
-            for col in range(9):
-                cell = Cell(row, col)
-                self.cells[row][col] = cell
+        for row, col in itertools.product(range(9), repeat=2):
+            cell = Cell(row, col)
+            self.cells[row, col] = cell
         self.boxes = {}
-        for box_row in range(3):
-            self.boxes[box_row] = {}
-            for box_col in range(3):
-                box = GridLayout(rows=3, cols=3, spacing=2)
-                self.boxes[box_row][box_col] = box
-                self.add_widget(box)
-                for row in range(box_row * 3, box_row * 3 + 3):
-                    for col in range(box_col * 3, box_col * 3 + 3):
-                        box.add_widget(self.cells[row][col])
+        for box_row, box_col in itertools.product(range(3), repeat=2):
+            box = GridLayout(rows=3, cols=3, spacing=2)
+            self.boxes[box_row, box_col] = box
+            self.add_widget(box)
+            for row in range(box_row * 3, box_row * 3 + 3):
+                for col in range(box_col * 3, box_col * 3 + 3):
+                    box.add_widget(self.cells[row, col])
 
     def get_cells(self, text):
-        for row in self.cells.values():
-            for cell in row.values():
-                if cell.digit.text == text:
-                    yield cell
+        for cell in self.cells.values():
+            if cell.digit.text == text:
+                yield cell
 
     def new_game(self):
         initial_alpha_shift = 0.8
@@ -169,21 +166,20 @@ class Board(GridLayout):
             [7, 0, 3, 4, 0, 0, 5, 6, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]
-        for i, row in enumerate(new_board):
-            for j, num in enumerate(row):
-                cell = self.cells[i][j]
-                cell.clean()
-                if cell.initial:
-                    cell.bg_color[0] /= initial_alpha_shift
-                    cell.bg_color[1] /= initial_alpha_shift
-                    cell.bg_color[2] /= initial_alpha_shift
-                    cell.initial = False
-                if num > 0:
-                    cell.digit.text = str(num)
-                    cell.bg_color[0] *= initial_alpha_shift
-                    cell.bg_color[1] *= initial_alpha_shift
-                    cell.bg_color[2] *= initial_alpha_shift
-                    cell.initial = True
+        for (row, col), cell in self.cells.items():
+            cell.clean()
+            if cell.initial:
+                cell.bg_color[0] /= initial_alpha_shift
+                cell.bg_color[1] /= initial_alpha_shift
+                cell.bg_color[2] /= initial_alpha_shift
+                cell.initial = False
+            num = new_board[row][col]
+            if num > 0:
+                cell.digit.text = str(num)
+                cell.bg_color[0] *= initial_alpha_shift
+                cell.bg_color[1] *= initial_alpha_shift
+                cell.bg_color[2] *= initial_alpha_shift
+                cell.initial = True
 
     def solve(self):
         pass
